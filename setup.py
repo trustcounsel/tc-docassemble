ba488cdf73b50567e06e25ccfda38ac8f2187bab
root <your@email.com>
2026-06-07 10:38:54 +0000

    Move installable docassemble package to repo root

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='docassemble.TrustCounselEstate',
+    version='0.0.1',
+    description='TrustCounsel Estate Planning Interviews',
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    install_requires=[],
+    zip_safe=False,
+)
